740cc813dedb1fac512baae80caf6b70a498ee97
UMAR <139427461+MrUmarDev@users.noreply.github.com>
2023-12-10 18:46:01 +0500

    Delete 3imx directory

--- 3imx/sources/database/index.js
+++
@@ -1,7 +0,0 @@
-const kx= require("knex");
-
-const knexFile = require("../../knexfile");
-
-const knex = kx(knexFile["development"]);
-
-module.exports = {knex};
--- 3imx/sources/routes/index.js
+++
@@ -1,9 +0,0 @@
-const authRouter = require("./auth.route");
-const studentRouter = require("./student.route");
-const teacherRouter = require("./teacher.route");
-
-module.exports = [
-    authRouter,
-    studentRouter,
-    teacherRouter,
-];
